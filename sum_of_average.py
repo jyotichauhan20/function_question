2f85af551362ecd07af7ed52ff404afcd656621d
jyotichauhan20 <72217749+jyotichauhan20@users.noreply.github.com>
2020-11-25 05:53:18 -0800

    average_questoion

---
+++ sum_of_average.py
@@ -0,0 +1,12 @@
+def sum_of_average(num1,num2,num3):
+    sum=num1+num2+num3
+    print(sum)
+    average=sum//sum
+    print(average)
+num1=int(input("enter the nuber="))
+num2=int(input("enter the number="))
+num3=int(input("enter yhe number="))
+sum_of_average(num1,num2,num3)
+        
+
+        
