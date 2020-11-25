cc1601ca5756094069d852ab4d135c46497ca431
jyotichauhan20 <72217749+jyotichauhan20@users.noreply.github.com>
2020-11-25 05:51:20 -0800

    Add files via upload

---
+++ string_length_count.py
@@ -0,0 +1,9 @@
+def string_length(string1,string2):
+    if len(string1)> len(string2):
+        print("welcome")
+    else:
+        print("sonu,monu")
+string1=str(input("enter your string="))
+string2=str(input("enter the string="))
+string_length(string1,string2)        
+        
